60b231b49980e9c89414b008935098e755805cac
SutanjoyB <bsutanjoy@gmail.com>
2026-03-29 00:34:51 +0530

    Logic

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const url = 'https://open-weather13.p.rapidapi.com/fivedaysforcast?latitude=40.730610&longitude=-73.935242&lang=EN';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'd3f33ef297msh1d66f3fb0082a3dp1ae6b0jsn97ca457e2e2c',
-		'x-rapidapi-host': 'open-weather13.p.rapidapi.com',
-		'Content-Type': 'application/json'
-	}
-};
+const cityInput = document.querySelector(".city-input");
+const searchBtn = document.querySelector(".search-btn");
 
-fetch(url, options)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.error(error));
+searchBtn.addEventListener("click", () => {
+  if (cityInput.value.trim() != "") {
+    updateWeatherInfo()
+    cityInput.value = "";
+    cityInput.blur();
+  }
+});
+
+cityInput.addEventListener("keydown", (event) => {
+  if (event.key == "Enter" && cityInput.value.trim() != "") {
+    updateWeatherInfo()
+    cityInput.value = "";
+    cityInput.blur(); 
+  }
+});
